f15aaad9f7878f034b08347c233befca52f4a77a
josercc <josercc@163.com>
2017-08-04 21:50:12 +0800

    初始化了工程

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// Generated automatically by Perfect Assistant Application
+// Date: 2017-08-04 13:42:47 +0000
+import PackageDescription
+let package = Package(
+	name: "PerfectCheWeiApi",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
+	]
+)
